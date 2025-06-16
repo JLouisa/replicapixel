48da03c58c145b67db288916a1e70bb63e7d5e2a
jlouisa <jonathan.louisa@gmail.com>
2025-06-16 19:01:11 +0200

    Setup db prod

--- migration/hello-prisma/i18n-script-save-2-db.ts
+++ migration/hello-prisma/i18n-script-save-2-db.ts
@@ -8,22 +8,37 @@ const prisma = new PrismaClient();
 async function importTranslationsFromJSON() {
   // Parse the files to get the data array
   const plans_json: any[] = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, "plans_translated.json"), "utf-8")
+    fs.readFileSync(path.resolve(__dirname, "plans.json"), "utf-8")
   );
 
   const packs_json: any[] = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, "packs_translated.json"), "utf-8")
+    fs.readFileSync(path.resolve(__dirname, "packs.json"), "utf-8")
+  );
+
+  const plansTranslations: any[] = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "plans_translations.json"), "utf-8")
+  );
+
+  const packsTranslations: any[] = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "packs_translations.json"), "utf-8")
   );
 
   // Sort by IDs to insert in a deterministic order
-  plans_json.sort((a: number, b: number) => a.plan_id - b.plan_id);
-  packs_json.sort((a: number, b: number) => a.pack_id - b.pack_id);
+  plans_json.sort((a: number, b: number) => a.id - b.id);
+  packs_json.sort((a: number, b: number) => a.id - b.id);
+  plansTranslations.sort((a: number, b: number) => a.pack_id - b.pack_id);
+  packsTranslations.sort((a: number, b: number) => a.pack_id - b.pack_id);
 
   // Insert into the database
-  await prisma.plans_translations.createMany({ data: plans_json });
+  await prisma.plans.createMany({ data: plans_json });
+  console.log("Plans successfully imported.");
+  await prisma.packs.createMany({ data: packs_json });
+  console.log("packs successfully imported.");
+
+  await prisma.plans_translations.createMany({ data: plansTranslations });
   console.log("Plans translations successfully imported.");
 
-  await prisma.packs_translations.createMany({ data: packs_json });
+  await prisma.packs_translations.createMany({ data: packsTranslations });
   console.log("Packs translations successfully imported.");
 
   console.log("Import finished.");
